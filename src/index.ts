b59aa1ae3c547a572a3fcd68e799fa43eae98fdf
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-22 18:42:10 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+
+
+let ingresados: boolean = false;
+let nro = Number(prompt("Ingrese un número"));
+let maximo: number = nro;
+let minimo: number = nro;
+let media : number = 0;
+let total : number = 0;
+let contador : number = 0;
+
+while (nro !== 0) {
+  ingresados = true;
+  if (nro > maximo) {
+    maximo = nro;
+  }if (nro < minimo) { 
+    minimo = nro;
+  }
+  total= total + nro;
+  contador++;
+  nro = Number(prompt("Ingrese un número"));
+  }
+  if (contador > 0 ){
+  media = total/contador;
+  }
+
+}
+if (ingresados) {
+  console.log("El número máximo es "+ maximo);
+  console.log ("El número mínimo es " + minimo);
+  console.log ("El promedio es " + media);
+}
